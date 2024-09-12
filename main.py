6f6af21fccb2a820459759c23449cdb95531294c
batnthn <batnthn@yahoo.com>
2024-09-11 22:51:14 -0400

    moving steps

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y= SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.update()
+            
+        screen.fill("black")
+        player.draw(screen)
+        dt = (clock.tick(60) / 1000)
+
 
 if __name__ == "__main__":
     main()
